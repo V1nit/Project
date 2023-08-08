c3b0bd154be29a93e4b034d593f921dbfe022a3b
Vinit <vinit172001@gmail.com>
2023-08-09 01:09:56 +0530

    setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+
+
+def get_requirements(file_path:str)->List[str]:
+    
+    '''this function will return the requirement'''
+    
+    HYPEN_E_DOT = '-e .'
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "")for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)  
+            
+    return requirements
+
+setup(
+    name = 'mlproject',
+    version='0.0.1',
+    author='Vinit',
+    author_email='vinit172001@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
